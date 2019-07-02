d3d93fb96c273888c3db91c9a7b439c051e0b533
njskywalker <njskywalker@users.noreply.github.com>
2019-07-02 17:02:46 +0100

    **UNFINISHED** Vertical motion function + changes
    **UNFINISHED** New vert. motion function, changes to init_stimulus to call it (ONLY works on continuous task, and only for S.xy--I didn't do it for the noise S.xy_noise so I can compare dot positions after I make a response). 
    
    I can't get the dots to behave properly when they go out of the annulus. Will work on this tomorrow
    
    Also, I need to add in the exponential function, and the "step-wise" coherence. Right now it's just a tri-variable (it's moving up, down, or no y-axis movement)

--- init_stimulus.m
+++ init_stimulus.m
@@ -1,5 +1,5 @@
 function [S] = init_stimulus(S,discrete_trials,tconst,filter_on)
-% This function creates sequences of xy dot positions for discrete trial
+% PURPOSE: This function creates sequences of xy dot positions for discrete trial
 % version of rdk or continuous versions
 
 % Input: 
@@ -23,11 +23,17 @@ if discrete_trials == 1 || discrete_trials == 2 % If we've selected for discrete
         tconst.framerate,S.vp.noise_amplitude,S.mean_duration,S.sd_duration,S.vp.noise_function, S.vp.stim_function);
         S.pre_incoh_mo_coh = pre_incoh_mo_coh; 
         
-        S.xy{trial} = xy;  
+        %%% Neb PSEUDOCODE: Here, xy = apply_vertical_motion() (modifying
+        %%% all the XY coordinates for vertical motion)
+        % apply vertical motion
+        % xy = apply_vertical_motion(xy, 5, 1, 30, 5, 3);
+        % finally, assign the xy coordinates to the S structure
+        S.xy{trial} = xy;
     end
 else % for continuous rdk 
     for block = 1:numel(S.vp.condition_vec) % loop through all blocks 
-        % calculate xy position for each dot for entire block duration 
+        %%% Calculate xy position for *each* dot for *entire* block 
+        %%% duration (coherent and incoherent periods)
         [xy,coherence_frame,mean_coherence,pre,blocks_shuffled] = ...
         move_dots(discrete_trials,0,S.totalframes_per_block,S.ap_radius,...
         S.vp.coherence_sd,S.vp.direction,S.step,S.Nd,0,...
@@ -36,18 +42,26 @@ else % for continuous rdk
         S.vp.passbandfreq, S.vp.stopbandfreq,S.vp.passrip,S.vp.stopbandatten,...
         tconst.framerate,S.vp.noise_amplitude,S.mean_duration,S.sd_duration,S.vp.noise_function, S.vp.stim_function);
 
-        S.xy{block} = xy; 
-        S.mean_coherence{block} = mean_coherence; 
-        S.blocks_shuffled{block} = blocks_shuffled; 
-        S.coherence_frame{block} = coherence_frame; 
+        % assign what's been returned from move_dots() to S structure...
+        %%% Neb PSEUDOCODE: Here, xy = apply_vertical_motion(xy,...)
+        %%% (modifying all the XY coordinates for vertical motion)
+        % apply vertical motion
+        xy = apply_vertical_motion(xy, 30, 0, 20, 3, 120, 30, S.step, S.ap_radius);
+        % finally, assign the xy coordinates to the S structure
+        S.xy{block} = xy;
+        S.mean_coherence{block} = mean_coherence;
+        S.blocks_shuffled{block} = blocks_shuffled;
+        S.coherence_frame{block} = coherence_frame;
 
-        % copy mean coherenes and actual coherence on each frame because we alter
+        % copy mean coherence and actual coherence on each frame because we alter
         % them in stim present function 
         S.mean_coherence_org{block} = S.mean_coherence{block}; % mean coherence
         S.coherence_frame_org{block} = S.coherence_frame{block}; % actual coherence used, 
         % oscillates with sd around mean coherence 
 
-        % calculate noise vector for replacing coherent motion after button press
+        %%% Calculate noise vector for replacing coherent motion after 
+        %%% button press (Neb: replaces coherent/incoherent motion vector 
+        %%% after a response in a trial?)
         noise_vec{block} = S.totalframes_per_block;
 
         [xy,coherence_frame,mean_coherence] = ...
@@ -58,9 +72,11 @@ else % for continuous rdk
         S.vp.passbandfreq, S.vp.stopbandfreq,S.vp.passrip,S.vp.stopbandatten,...
         tconst.framerate,S.vp.noise_amplitude,S.mean_duration,S.sd_duration,S.vp.noise_function, S.vp.stim_function);
 
+        %%% Neb PSEUDOCODE: Here, xy = apply_vertical_motion() (modifying
+        %%% all the XY noise coordinates for vertical motion)
         S.xy_noise{block} = xy;
-        % copy mean coherenes and actual coherence on each frame because we alter
-        % them in stim present function 
+        % copy mean coherences and actual coherence on each frame because we alter
+        % them in stim present function
         S.mean_coherence_noise{block} = mean_coherence; 
         S.coherence_frame_noise{block} = coherence_frame; 
     end % loop through blocks
